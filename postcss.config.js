39818db9a6431c1789af3a3df44328c1e775ed78
Alexander Capehart <alex@oxycblt.org>
2025-03-28 09:52:35 -0600

    Break up content css
    Turns out you can dynamically link it with a special htmx trick

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('postcss-nested'),
-        require('cssnano')({
-            preset: 'default'
-        })
-    ]
-};
